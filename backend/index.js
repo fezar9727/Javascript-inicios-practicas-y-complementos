fc1b918d6aee1565f0315d912789004f1b8a9086
fezar9727 <fezar.2020@gmail.com>
2026-06-16 04:41:59 -0500

    Se creo la interfaz de registro en HTML, la logica del componente y el servicio usando TypeScript en Angular; ademas se configuro el middleware CORS en la carpeta del backend para permitir la comunicacion entre ambos proyectos

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,17 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const auth = require('./routes/auth.routes');
+const task = require('./routes/task.routes'); // Importa las rutas relacionadas con la tarea de task-crud-mongoose
+const cors = require('cors');
 
-const taskRoutes = require('./routes/task.routes'); // Importa las rutas relacionadas con la tarea de task-crud-mongoose
 
 dotenv.config();
 connectDB();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', auth)
-
-app.use('/api/task', taskRoutes); // <-- Registra las rutas de la tarea de task-crud-mongoose bajo el endpoint /api/task
+app.use('/api/task', task); // <-- Registra las rutas de la tarea de task-crud-mongoose bajo el endpoint /api/task
 
 const PORT = process.env.PORT;
 
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -9,5 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideHttpClient(),
+    importProvidersFrom(ReactiveFormsModule)
   ]
 };
